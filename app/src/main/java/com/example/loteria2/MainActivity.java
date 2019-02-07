3283ffa28a9a57c9f78e31fb99075730f94c2898
sarca <sarcasmo1601@gmail.com>
2019-02-07 07:13:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/loteria2/MainActivity.java
@@ -0,0 +1,244 @@
+package com.example.loteria2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    int con=0;
+    ImageView im, pi;
+    TextView tx;
+    Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void imagen1(View v){
+        con++;
+        im= findViewById(R.id.im1);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra1);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen2(View v){
+        con++;
+        im= findViewById(R.id.im2);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra2);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen3(View v){
+        con++;
+        im= findViewById(R.id.im3);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra3);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen4(View v){
+        con++;
+        im= findViewById(R.id.im4);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra4);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen5(View v){
+        con++;
+        im= findViewById(R.id.im5);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra5);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen6(View v){
+        con++;
+        im= findViewById(R.id.im6);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra6);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen7(View v){
+        con++;
+        im= findViewById(R.id.im7);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra7);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen8(View v){
+        con++;
+        im= findViewById(R.id.im8);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra8);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen9(View v){
+        con++;
+        im= findViewById(R.id.im9);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra9);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen10(View v){
+        con++;
+        im= findViewById(R.id.im10);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra10);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen11(View v){
+        con++;
+        im= findViewById(R.id.im11);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra11);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen12(View v){
+        con++;
+        im= findViewById(R.id.im12);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra12);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen13(View v){
+        con++;
+        im= findViewById(R.id.im13);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra13);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen14(View v){
+        con++;
+        im= findViewById(R.id.im14);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra14);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen15(View v){
+        con++;
+        im= findViewById(R.id.im15);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra15);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void imagen16(View v){
+        con++;
+        im= findViewById(R.id.im16);
+        im.setEnabled(false);
+        pi=findViewById(R.id.piedra16);
+        pi.setBackgroundResource(R.drawable.piedra);
+        if(con==16){
+            tx=findViewById(R.id.txt1);
+            tx.setText("LOTERIA");
+            bt = findViewById(R.id.btn1);
+            bt.setEnabled(true);
+        }
+    }
+    public void boton(View v){
+        bt= findViewById(R.id.btn1);
+        bt.setEnabled(false);
+        findViewById(R.id.im1).setEnabled(true);
+        findViewById(R.id.im2).setEnabled(true);
+        findViewById(R.id.im3).setEnabled(true);
+        findViewById(R.id.im4).setEnabled(true);
+        findViewById(R.id.im5).setEnabled(true);
+        findViewById(R.id.im6).setEnabled(true);
+        findViewById(R.id.im7).setEnabled(true);
+        findViewById(R.id.im8).setEnabled(true);
+        findViewById(R.id.im9).setEnabled(true);
+    }
+}
